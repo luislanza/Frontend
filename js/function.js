c6bc71cc74b076fedc56811b67891554760ce4c7
luislanza <luislanzza@gmail.com>
2023-02-25 18:26:16 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,7 @@
+document.getElementById('masInfoComp').addEventListener('click',function() {
+    window.open('competencias.html','_self');
+});
+
+document.getElementById('masInfoTit').addEventListener('click',function() {
+    window.open('titulacion.html','_self');
+    });
